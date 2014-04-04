c79058c60c5e60bda2116857f1f2b288d92f9e73
originalblend337 <originalblend337@gmail.com>
2014-04-03 23:11:36 -0400

    delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,7 +14,11 @@ class CommentsController < ApplicationController
 	end
 
 	def destroy
-		
+		@comment = Comment.find(params[:id])
+		@post = @comment.post
+		@comment.destroy
+
+		redirect_to @post
 	end
 
 	private
