54d86d96637baf8698c622e4a7ef671efbf0b9d1
originalblend337 <originalblend337@gmail.com>
2014-04-23 10:00:12 -0400

    adjustments for keybindings

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,6 +1,11 @@
+//SET WIDEAREA AND HIDE FORM
 wideArea().setOption("defaultColorScheme", "dark");
 $('form').hide();
 
+//KEY-BINDINGS FOR FORM
 Mousetrap.bind('command+i', function() {
-	$('.widearea-icons a').click();
+	$('.new_form .widearea-icons a').click();
+});
+Mousetrap.bind('command+enter', function() {
+	$('form.new_form :submit').click();
 });
